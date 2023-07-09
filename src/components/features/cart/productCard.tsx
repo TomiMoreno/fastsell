756d59f1a3bd2ff2344ce0794d21e42ca1ee211d
TomiMoreno <tomimoreno03@gmail.com>
2023-07-09 12:39:55 -0300

    add search input

--- src/components/features/cart/productCard.tsx
+++ src/components/features/cart/productCard.tsx
@@ -23,7 +23,7 @@ export default function ProductCard({
   amount,
 }: ProductCardProps) {
   return (
-    <Card className="cursor-pointer hover:shadow-lg">
+    <Card className="spac flex cursor-pointer flex-row hover:shadow-lg">
       <CardHeader className="flex flex-row items-center justify-between">
         <div className="space-y-1">
           <CardTitle className="text-xl">{product.name}</CardTitle>
--- src/components/features/cart/productGrid.tsx
+++ src/components/features/cart/productGrid.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import React, { type ChangeEvent } from "react";
+import React, { useState, type ChangeEvent, useRef } from "react";
 import { Button } from "~/components/ui/button";
 import { api } from "~/utils/api";
 import ProductCard from "./productCard";
 import { useCart } from "~/lib/store";
 import { useToast } from "~/components/ui/use-toast";
+import { Input } from "~/components/ui/input";
 
 function ProductGrid() {
+  const [seacrh, setSeacrh] = useState("");
+  const ref = useRef<HTMLInputElement>(null);
   const { toast } = useToast();
   const { data } = api.product.getAll.useQuery();
   const context = api.useContext();
@@ -24,6 +27,8 @@ function ProductGrid() {
     toast({
       title: "Compra realizada",
     });
+    setSeacrh("");
+    ref.current?.focus();
   };
 
   if (isLoading) {
@@ -33,20 +38,29 @@ function ProductGrid() {
     <div className="container mx-auto py-10">
       <h1 className="text-3xl font-bold">Productos</h1>
       <h2 className="fixed right-0 top-0 text-xl font-bold">Total: {total}</h2>
-      <div className="max-w-xxl grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {data?.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            plusOne={() => addToCart(product, product.price)}
-            minusOne={() => removeFromCart(product, product.price)}
-            handleChange={(e: ChangeEvent<HTMLInputElement>) => {
-              const value = parseInt(e.target.value);
-              changeAmount(product, product.price, value);
-            }}
-            amount={items.get(product.id)?.amount ?? 0}
-          />
-        ))}
+      <Input
+        value={seacrh}
+        onChange={(e) => setSeacrh(e.target.value)}
+        ref={ref}
+      />
+      <div className="max-w-xxl grid grid-cols-1">
+        {data
+          ?.filter((product) =>
+            product.name.toLowerCase().includes(seacrh.toLowerCase())
+          )
+          .map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              plusOne={() => addToCart(product, product.price)}
+              minusOne={() => removeFromCart(product, product.price)}
+              handleChange={(e: ChangeEvent<HTMLInputElement>) => {
+                const value = parseInt(e.target.value);
+                changeAmount(product, product.price, value);
+              }}
+              amount={items.get(product.id)?.amount ?? 0}
+            />
+          ))}
         <Button
           variant="default"
           className="fixed bottom-0 col-span-full w-full"
