75157aa2b93a45c9975a7939f159ee7c75aedef5
Tomás Canosa Moreno <tomimoreno03@gmail.com>
2023-10-08 02:38:29 -0300

    Add sound to kbd component

--- src/components/ui/kbd.tsx
+++ src/components/ui/kbd.tsx
@@ -12,9 +12,14 @@ const Kbd = React.forwardRef<HTMLElement, React.HTMLAttributes<HTMLElement>>(
       <kbd
         ref={ref}
         className={cn(
-          "relative top-[-1px] block min-w-[0.75rem] cursor-default rounded-sm border border-foreground/10 bg-transparent px-[5px] py-[2px] text-center text-base leading-[1] shadow-[0_2px_0_1px_hsl(var(--foreground)_/_0.1)] hover:top-[1px] hover:shadow-[0_1px_0_0.05px_hsl(var(--foreground)_/_0.1)]",
+          "relative top-[-1px] block min-w-[0.75rem] cursor-default rounded-sm border border-foreground/10 bg-transparent px-[5px] py-[2px] text-center font-mono text-base leading-[1] shadow-[0_2px_0_1px_hsl(var(--foreground)_/_0.1)] active:top-[1px] active:shadow-[0_1px_0_0.05px_hsl(var(--foreground)_/_0.1)]",
           className
         )}
+        onClick={() => {
+          // Reproduce sound
+          const audio = new Audio("/sounds/click.wav");
+          audio.play().catch((err) => console.error(err));
+        }}
         {...props}
       />
     );
