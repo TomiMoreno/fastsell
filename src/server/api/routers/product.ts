f59302cd03824baddeddee990fa3923be7da8f65
Tomás Canosa Moreno <tomimoreno03@gmail.com>
2024-05-08 23:47:14 -0300

    Update routers to use drizzle

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -1,44 +1,42 @@
+import { eq } from "drizzle-orm";
 import { z } from "zod";
 import {
   createProductSchema,
   updateProductSchema,
 } from "~/lib/schemas/product";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { productsTable } from "~/server/schema";
 
 export const productRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.product.findMany();
+    return ctx.db.select().from(productsTable).all();
   }),
   create: publicProcedure
     .input(createProductSchema)
-    .mutation(async ({ ctx, input }) => {
-      const product = await ctx.prisma.product.create({
-        data: {
+    .mutation(async ({ ctx, input }) =>
+      ctx.db
+        .insert(productsTable)
+        .values({
           name: input.name,
           price: input.price,
           stock: input.stock,
           image: "https://picsum.photos/200",
-        },
-      });
-      return product;
-    }),
+          hotkey: null,
+        })
+        .returning()
+    ),
   update: publicProcedure
     .input(updateProductSchema)
     .mutation(({ ctx, input }) =>
-      ctx.prisma.product.update({
-        where: {
-          id: input.id,
-        },
-        data: {
+      ctx.db
+        .update(productsTable)
+        .set({
           ...input,
-        },
-      })
+        })
+        .where(eq(productsTable.id, input.id))
+        .returning()
     ),
   delete: publicProcedure.input(z.string()).mutation(({ ctx, input }) => {
-    return ctx.prisma.product.delete({
-      where: {
-        id: input,
-      },
-    });
+    return ctx.db.delete(productsTable).where(eq(productsTable.id, input));
   }),
 });
