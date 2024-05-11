e233b522093e045cb636202887debc58fa01c19e
thiago <thifran789456323@gmail.com>
2024-05-11 01:36:30 -0300

    use sqliteTableCreator

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from "drizzle-kit";
-import { env } from "~/env.mjs";
+import { env } from "~/env.js";
 
 export default {
   schema: "./src/server/db/schema.ts",
   out: "./drizzle/migrations",
+  dialect: "sqlite",
   driver: "turso",
   dbCredentials: {
     url: env.TURSO_DATABASE_URL,
     authToken: env.TURSO_AUTH_TOKEN,
   },
+  tablesFilter: ["fastsell_*"],
 } satisfies Config;
