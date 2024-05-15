cef459aefc6658d79561cea8f4a9f597730b7e3c
Tomás Canosa Moreno <tomimoreno03@gmail.com>
2024-05-14 23:59:51 -0300

    Change login page styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,77 +1,37 @@
-"use client";
+import Image from "next/image";
+import Link from "next/link";
 
-import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { api } from "~/trpc/react";
-import { useRouter } from "next/navigation";
+import LoginForm from "./login-form";
 
-const schema = z.object({
-  email: z.string().email("Ingresa un Email correcto"),
-  password: z
-    .string()
-    .min(8, {
-      message: "El password tiene que ser de minimo 8 caracteres",
-    })
-    .max(20, {
-      message: "Pasaste el limite de 20 caracteres",
-    }),
-});
-export default function Login() {
-  const router = useRouter();
-  const signIn = api.auth.signIn.useMutation({
-    onSuccess() {
-      router.push("/");
-    },
-  });
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<z.infer<typeof schema>>({
-    resolver: zodResolver(schema),
-  });
-
+export default function LoginPage() {
   return (
-    <main className="flex grow flex-col items-center justify-start bg-background">
-      <div className="mt-[60px]">
-        <div className="flex flex-col gap-7 text-center">
-          <h1 className="text-3xl font-extrabold tracking-tight text-secondary-foreground sm:text-[5rem]">
-            FastSell
-          </h1>
-          <p className="text-xl text-secondary-foreground">
-            Logueate para continuar!
-          </p>
-
-          <form onSubmit={handleSubmit((data) => signIn.mutate(data))}>
-            <div className="flex flex-col gap-2">
-              <Input
-                type="email"
-                placeholder="Email"
-                required
-                {...register("email")}
-              />
-              <Input
-                type="password"
-                placeholder="Password"
-                required
-                {...register("password")}
-              />
-              <Button variant={"accent"} type="submit">
-                Iniciar sesión
-              </Button>
-            </div>
-          </form>
-          <div className="text-sm font-medium text-destructive">
-            {JSON.stringify(errors.email?.message)}
+    <div className="min-h-screen w-full lg:grid lg:grid-cols-2">
+      <div className="flex items-center justify-center py-12 ">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Inicia sesión</h1>
+            <p className="text-balance text-muted-foreground">
+              Ingresa tu tu email y contraseña para continuar
+            </p>
           </div>
-          <div className="text-sm font-medium text-destructive">
-            {JSON.stringify(errors.password?.message)}
+          <LoginForm />
+          <div className="mt-4 text-center text-sm">
+            No tienes una cuenta?{" "}
+            <Link href="/register" className="underline">
+              Registrate
+            </Link>
           </div>
         </div>
       </div>
-    </main>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/images/login-background.png"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.7] "
+        />
+      </div>
+    </div>
   );
 }
